7d7faf5d00a4838923568b10e17bb2ce7f75000d
leeq <7leeq7@gmail.com>
2024-05-07 02:16:32 +0300

    Addition NoteColor.

--- app/src/main/java/com/topic2/android/notes/ui/components/NoteColor.kt
+++ app/src/main/java/com/topic2/android/notes/ui/components/NoteColor.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
@@ -19,21 +18,20 @@ import androidx.compose.ui.unit.Dp
 
 @Composable
 fun NoteColor(
+    modifier: Modifier = Modifier,
     color: Color,
     size: Dp,
-    padding: Dp = 0.dp,
     border: Dp
 ) {
     Box(
-        modifier = Modifier
-            .padding(padding)
+        modifier = modifier
             .size(size)
             .clip(CircleShape)
             .background(color)
 
             .border(
                 BorderStroke(
-                    2.dp,
+                    border,
                     SolidColor(Color.Black)
                 ),
                 CircleShape
@@ -44,5 +42,5 @@ fun NoteColor(
 @Preview
 @Composable
 fun NoteColorPreview() {
-    NoteColor(color = Color.Red, size = 40.dp, padding = 4.dp, border = 2.dp)
+    NoteColor(color = Color.Red, size = 40.dp, border = 2.dp)
 }
